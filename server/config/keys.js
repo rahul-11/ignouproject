5f4df690df027dce2a43f5d91b442533391987ae
Rahul Gupta <rgupta8800368113@gmail.com>
2019-04-16 12:21:57 +0530

    added k8s files

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,15 +1,17 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = require('./prod');
-}
-if(process.env.NODE_ENV === 'ci'){
-  module.exports = {
-    mongoURI: "mongodb://mongodb:27017/worthero",
-    secret: "ncdrejbgklrcibewriobfklaca"
+  if(process.env.NODE_ENV){
+  if(process.NODE_ENV == 'prod'){
+    module.exports = require('./prod');
   }
+  
+  else if(process.env.NODE_ENV = 'ci'){
+    module.exports = {
+      mongoURI: process.env.MONGOURI,
+      secret: "scewbriugtv877tg4ibtg0ye32h3rof"
+    }
+  }
+
 }
-else {
+else{
   module.exports = require('./dev');
 }
-   
-
 
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,15 @@
 module.exports ={
-  mongoURI: "mongodb://127.0.0.1:27017/worthero",
-    secret: "ncdrejbgklrcibewriobfklaca"
+  mongoURI: `mongodb://${process.env.MONGOHOST}:${process.env.MONGOPORT}/${process.env.MONGODBNAME}`,
+  secret: "ncdrejbgklrcibewriobfklaca"
 }
+
+/* 
+  format of mongoDB uri:  
+  mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+  example:
+  `mongodb://
+    ${process.env.MONGOUSER}:${process.env.MONGOPASS}
+    @${process.env.MONGOHOST}:${process.env.MONGOPORT}
+    /${process.env.DBNAME}`
+
+ */
